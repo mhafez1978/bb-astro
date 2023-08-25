d92df0b680c148813f6463bb8744c0a400f1eea2
Mohamed Hafez <mhafez1978@outlook.com>
2023-08-24 23:01:52 -0400

    made some changes to routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  outDir: "cpanel",
 });
---
+++ cpanel/pages/posts.astro.mjs
@@ -0,0 +1,8 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import '../chunks/astro.c35989ab.mjs';
+import 'html-escaper';
+
+const page = () => import('../chunks/pages/index.astro.0562b8f4.mjs').then(n => n.f);
+
+export { page };
